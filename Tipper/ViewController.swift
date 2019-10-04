399de55c5a4dd99e551eb7309b0105e4a527445c
Carlos Mendoza <1cmendoza1996@gmail.com>
2019-10-04 18:07:33 -0400

    Initial Commit

---
+++ Tipper/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tipper
+//
+//  Created by Carlos Mendoza on 10/4/19.
+//  Copyright © 2019 Carlos Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
